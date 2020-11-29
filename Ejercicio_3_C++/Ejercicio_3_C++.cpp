2f5d818d92c351e8fa761d8bf82457f174d18bf3
Meliodas-R <65538839+Meliodas-R@users.noreply.github.com>
2020-11-29 17:18:05 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio_3_C++/Ejercicio_3_C++.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/*
+* 3. Pedir al usuario que introduzca un número por teclado y después mostrarlo por pantalla.
+* 
+*
+*/
+int main()
+{
+	int numero;
+
+	cout << "Introduzca un numero: ";
+	cin >> numero;
+
+	cout << "\nEl numero que has introducido es: " << numero;
+
+	return 0;
+}
